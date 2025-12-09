f5e2b11795fa0839d6dcee61f98c322fc908df00
Wataru198 <ttc22c1010001@edu.tech.ac.jp>
2025-12-09 14:21:40 +0900

    Add Day1 count app

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// Reactの機能を使うためにReactをインポートする
+import React, { useState } from "react";
 
+// アプリ全体を表すコンポーネントAppを定義する
 function App() {
+  // countという状態（数字）と、その値を更新するsetCount関数を用意する
+  // useState(0) で、最初の値を0にする
+  const [count, setCount] = useState(0);
+
+  // ボタンがクリックされたときに実行される処理を定義する
+  const handleClick = () => {
+    // setCountを使ってcountの値を1増やす
+    setCount(count + 1);
+  };
+
+  // 画面に表示する内容を返す
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // アプリ全体を囲むdivタグ
+    <div>
+      {/* アプリのタイトルを表示する */}
+      <h1>Day1 カウントアプリ</h1>
+
+      {/* 現在のカウントの値を表示する */}
+      <p>現在のカウント値: {count}</p>
+
+      {/* クリックするとhandleClickが実行されるボタン */}
+      <button onClick={handleClick}>＋1する</button>
     </div>
   );
 }
 
+// 他のファイルからこのAppコンポーネントを使えるようにする
 export default App;
