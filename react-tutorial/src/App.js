aa61cd50dd5c4071ebe1fe7e96ffbf0dd298ec77
Wataru198 <ttc22c1010001@edu.tech.ac.jp>
2025-12-09 14:43:14 +0900

    Add decrement button to Day1 count app

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,4 +1,4 @@
-// Reactの機能を使うためにReactをインポートする
+// Reactの機能を使うためにReactとuseStateをインポートする
 import React, { useState } from "react";
 
 // アプリ全体を表すコンポーネントAppを定義する
@@ -7,10 +7,16 @@ function App() {
   // useState(0) で、最初の値を0にする
   const [count, setCount] = useState(0);
 
-  // ボタンがクリックされたときに実行される処理を定義する
-  const handleClick = () => {
+  // ＋ボタンがクリックされたときに実行される処理を定義する
+  const handleIncrement = () => {
     // setCountを使ってcountの値を1増やす
     setCount(count + 1);
+  };
+
+  // −ボタンがクリックされたときに実行される処理を定義する
+  const handleDecrement = () => {
+    // setCountを使ってcountの値を1減らす
+    setCount(count - 1);
   };
 
   // 画面に表示する内容を返す
@@ -23,8 +29,11 @@ function App() {
       {/* 現在のカウントの値を表示する */}
       <p>現在のカウント値: {count}</p>
 
-      {/* クリックするとhandleClickが実行されるボタン */}
-      <button onClick={handleClick}>＋1する</button>
+      {/* クリックするとhandleIncrementが実行されるボタン */}
+      <button onClick={handleIncrement}>＋1する</button>
+
+      {/* クリックするとhandleDecrementが実行されるボタン */}
+      <button onClick={handleDecrement}>−1する</button>
     </div>
   );
 }
